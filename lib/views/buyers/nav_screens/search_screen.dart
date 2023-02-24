317e493e488ce9d645a5115ca58754adbd0306a3
Melvin Liew <melvin@testemail.com>
2023-02-24 20:30:46 +0800

    add basic contents

---
+++ lib/views/buyers/nav_screens/search_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Search screen'),
+    );
+  }
+}
